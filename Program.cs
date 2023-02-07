e837c348bfe11c3e03756a5bb52327e5704ad9e7
Sofia-Pedraza7 <122932004+Sofia-Pedraza7@users.noreply.github.com>
2023-02-06 22:34:20 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,39 @@
-﻿namespace Homework1;
+﻿namespace Homework3;
 class Program
 {
+    
     static void Main(string[] args)
     {
-        double X = 2.5;
-        double Y = 3.3;
-        double Z = 4*X*X+3*Y;
-        Console.WriteLine(Z);
+      Console.WriteLine("-----Question 1-----");
+            Console.WriteLine("Input an integer:");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number < 2)
+            {
+                Console.WriteLine("N is non-prime");
+                return; // to end the program
+
+            }
+
+            bool isPrime = true;
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine("N is prime");
+            }
+            else
+            {
+                Console.WriteLine("N is non-prime");
+            }
+        }
     }
-}
+
