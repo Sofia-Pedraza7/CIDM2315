e5d1046692b2693e0181e2fb3504e8af3cfd6f36
Sofia-Pedraza7 <122932004+Sofia-Pedraza7@users.noreply.github.com>
2023-02-06 23:04:09 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,8 @@
 ﻿namespace Homework3;
 class Program
 {
+    
     static void Main(string[] args)
-    {
-        for(int row=0; row<5; row++){
-            for(int col = 0; col<5; col++){
-                Console.Write('#');
-            }
-            Console.WriteLine("");
-        }
-    }
-}
+    {}
+      
+
