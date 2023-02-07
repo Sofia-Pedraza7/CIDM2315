583ecf7ff22cd30347c13db9a818ef765f4e11a9
Sofia-Pedraza7 <122932004+Sofia-Pedraza7@users.noreply.github.com>
2023-02-06 23:15:54 -0600

    Homework 3.2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿namespace Homework3;
+﻿namespace Homework3._2;
 class Program
 {
     static void Main(string[] args)
     {
-        for(int row=0; row<5; row++){
-            for(int col = 0; col<5; col++){
-                Console.Write('#');
+        for (int row = 0; row < 5; row++)
+        {
+            for (int col = 0; col < 5; col++)
+            {
+                if (row >= col)
+
+                    Console.Write('*');
             }
             Console.WriteLine("");
         }
